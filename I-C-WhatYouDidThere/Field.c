095880028241a3cc0b3c935ac4b0bfed54750132
Ripiter <44769203+Ripiter@users.noreply.github.com>
2021-04-23 15:27:32 +0200

    Fixing place ship

--- I-C-WhatYouDidThere/Field.c
+++ I-C-WhatYouDidThere/Field.c
@@ -1,10 +1,10 @@
 #include "Field.h"
 
-void assign_ship_to_field(Field_t field, Ship_t ship)
+void assign_ship_to_field(Field_t *field, Ship_t ship)
 {
-	field.ship = ship;
-	field.ship.pos = field.pos;
-	field.ship.init = 1;
+	field->ship = ship;
+	field->ship.pos = field.pos;
+	field->ship.init = 1;
 
-	field.fieldChar = ship.shipChar;
+	field->fieldChar = ship.shipChar;
 }
